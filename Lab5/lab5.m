6f0843ca619e9956fd8bb10400ad9d42fce91958
guzungrigore <113361340+guzungrigore@users.noreply.github.com>
2023-05-16 14:00:55 +0300

    Added Lab 5

--- Lab5/lab5.m
+++ Lab5/lab5.m
@@ -40,48 +40,7 @@ function [distances, path] = floyd_warshall(graph)
     end
 end
 
-% Graphs with 4 nodes
-sparse_graph = zeros(4, 4);
-sparse_graph(1, 2) = 10;
-sparse_graph(2, 4) = 5;
-sparse_graph(3, 4) = 9;
-
-dense_graph = ones(4, 4) - eye(4);
-dense_graph(1, 2) = 10;
-dense_graph(2, 4) = 5;
-dense_graph(3, 4) = 9;
-
-% Graphs with 8 nodes
-n = 8;
-sparse_graph = zeros(n, n);
-sparse_graph(1, 2) = 3;
-sparse_graph(1, 3) = 7;
-sparse_graph(1, 6) = 2;
-sparse_graph(2, 3) = 2;
-sparse_graph(2, 4) = 4;
-sparse_graph(2, 6) = 6;
-sparse_graph(3, 4) = 5;
-sparse_graph(3, 5) = 10;
-sparse_graph(4, 5) = 3;
-sparse_graph(4, 7) = 8;
-sparse_graph(5, 7) = 2;
-sparse_graph(6, 7) = 5;
-
-dense_graph = ones(n, n) - eye(n);
-dense_graph(1, 2) = 3;
-dense_graph(1, 3) = 7;
-dense_graph(1, 6) = 2;
-dense_graph(2, 3) = 2;
-dense_graph(2, 4) = 4;
-dense_graph(2, 6) = 6;
-dense_graph(3, 4) = 5;
-dense_graph(3, 5) = 10;
-dense_graph(4, 5) = 3;
-dense_graph(4, 7) = 8;
-dense_graph(5, 7) = 2;
-dense_graph(6, 7) = 5;
-
-n = 1000; % number of vertices
+n = 150; % number of vertices
 p = 0.1; % probability of an edge in a sparse graph
 graph_sparse = sprand(n, n, p);
 graph_sparse(graph_sparse ~= 0) = randi([1, 10], nnz(graph_sparse), 1);
@@ -89,19 +48,19 @@ graph_dense = randi([0, 10], n, n);
 
 % Measure algorithm performance
 tic;
-[dijkstra_sparse_distances, dijkstra_sparse_path] = dijkstra(sparse_graph, 1);
+[dijkstra_sparse_distances, dijkstra_sparse_path] = dijkstra(graph_sparse, 1);
 dijkstra_sparse_time = toc;
 
 tic;
-[dijkstra_dense_distances, dijkstra_dense_path] = dijkstra(dense_graph, 1);
+[dijkstra_dense_distances, dijkstra_dense_path] = dijkstra(graph_dense, 1);
 dijkstra_dense_time = toc;
 
 tic;
-[floyd_sparse_distances, floyd_sparse_path] = floyd_warshall(sparse_graph);
+[floyd_sparse_distances, floyd_sparse_path] = floyd_warshall(graph_sparse);
 floyd_sparse_time = toc;
 
 tic;
-[floyd_dense_distances, floyd_dense_path] = floyd_warshall(dense_graph);
+[floyd_dense_distances, floyd_dense_path] = floyd_warshall(graph_dense);
 floyd_dense_time = toc;
 
 % Plot the running times of the algorithms for the sparse and dense graphs
